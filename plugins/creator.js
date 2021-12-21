001320edc66a4b6731fc3f63930413f8241cfaca
WhyMe? I'm Sorry :( <80481330+WolfCodeX29@users.noreply.github.com>
2021-12-21 19:22:47 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -3,9 +3,9 @@ async function handler(m) {
                 let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
                     + 'VERSION:3.0\n' 
                     + 'N:;Ilman;;;'
-                    + 'FN:Ilman\n' // full name
-                    + 'ORG:Ilman;\n' // the organization of the contact
-                    + 'TEL;type=CELL;type=VOICE;waid=6281351047727:+62 813-5104-7727\n' // WhatsApp ID + phone number
+                    + 'FN: WolfCodeX\n' // full name
+                    + 'ORG: WolfCodeX;\n' // the organization of the contact
+                    + 'TEL;type=CELL;type=VOICE;waid=62857785356307:+62 857 7853-56307\n' // WhatsApp ID + phone number
                     + 'END:VCARD'
                 conn.sendMessage(m.chat, { contacts: { displayName: 'Ilman', contacts: [{ vcard }] } }, { quoted: m })
 }
